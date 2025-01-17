ffaf5d987eaa3d8490c65160b895055a7d7073b9
Brian Hvarregaard <brian@median.dk>
2025-01-17 12:43:29 +0100

    Added Service Initialization

---
+++ uHelpDesk.DAL/SetupDAL.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uHelpDesk.DAL
+{
+    public static class SetupDAL
+    {
+        public static IServiceCollection InitializeDAL(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<uHelpDeskDbContext>(options =>
+    options.UseSqlServer(connectionString,
+                sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(10),
+                    errorNumbersToAdd: null)));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options => {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            })
+                .AddEntityFrameworkStores<uHelpDeskDbContext>();
+
+
+
+            //services.AddScoped<IAsyncInventoryBrandRepository, InventoryBrandRepository>();
+            return services;
+        }
+    }
+}
--- uHelpDesk.DAL/uHelpDeskDbContext.cs
+++ uHelpDesk.DAL/uHelpDeskDbContext.cs
@@ -1,7 +1,21 @@
-﻿namespace uHelpDesk.DAL
-{
-    public class uHelpDeskDbContext
-    {
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
+using uHelpDesk.Models;
 
+namespace uHelpDesk.DAL
+{
+    public class uHelpDeskDbContext : IdentityDbContext
+    {
+        public uHelpDeskDbContext(DbContextOptions<uHelpDeskDbContext> options)
+            : base(options)
+        {
+        }
+
+       public DbSet<Customer> Customers { get;set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<CustomField> CustomFields { get; set; }
+        public DbSet<CustomFieldValue> CustomFieldValues { get; set; }
+        public DbSet<TicketStatus> TicketStatuses { get; set; }
     }
 }
