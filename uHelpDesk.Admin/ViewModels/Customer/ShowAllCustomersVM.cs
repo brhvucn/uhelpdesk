a8d3c0eba61baa028e31d9d59325202c6a7c0e7f
Brian Hvarregaard <brian@median.dk>
2025-04-29 19:23:27 +0200

    Minor changes to layout and names

--- uHelpDesk.Admin/ViewModels/Customer/ShowAllCustomersVM.cs
+++ uHelpDesk.Admin/ViewModels/Customer/ShowAllCustomersVM.cs
@@ -1,7 +1,13 @@
 ﻿namespace uHelpDesk.Admin.ViewModels.Customer;
+
+using uHelpDesk.Admin.Utils;
 using uHelpDesk.Models;
 
-public class ShowAllCustomersVM
+public class ShowAllCustomersVM : BaseVM
 {
+    public ShowAllCustomersVM()
+    {
+        this.PageTitle = Names.CustomerNames.AllCustomers;
+    }
     public IList<Customer> Customers = new List<Customer>();
 }
