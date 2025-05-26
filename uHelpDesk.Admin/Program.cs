fc7284b7ec11a671f632fc8d9f24963e3b9fa66f
Lasselarsen4 <130568529+Lasselarsen4@users.noreply.github.com>
2025-05-26 17:11:24 +0200

    Update Program.cs

--- uHelpDesk.Admin/Program.cs
+++ uHelpDesk.Admin/Program.cs
@@ -8,18 +8,20 @@ using uHelpDesk.DAL;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                       ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<uHelpDeskDbContext>(options =>
     options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddControllersWithViews();
-//add custom services
+
+// Add custom services
 builder.Services.InitializeDAL(connectionString);
 builder.Services.InitializeBLL();
 builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddDbContext<uHelpDeskDbContext>(options
-    => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,15 +32,12 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
